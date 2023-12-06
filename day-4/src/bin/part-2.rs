c73531becf2c1de49b802863bda625dbe1e99056
Bojan Wilytsch <bwilytsch@gmail.com>
2023-12-06 06:15:27 +0000

    day 4 improve

--- day-4/src/bin/part-2.rs
+++ day-4/src/bin/part-2.rs
@@ -1,12 +1,11 @@
 use std::collections::HashSet;
 
-fn process(input: &str) -> i32 {
-    let mut played = vec![0; input.lines().count()];
+fn process(input: &str) -> Result<i32, &str> {
+    let mut played = vec![1; input.lines().count()];
 
     for (i, line) in input.lines().enumerate() {
         let result: Vec<&str> = line.split(":").collect();
         let cards: Vec<&str> = result[1].split("|").collect();
-        played[i] += 1;
 
         let winnum: HashSet<i32> = cards
             .first()
@@ -30,12 +29,14 @@ fn process(input: &str) -> i32 {
     }
 
     let sum = played.iter().sum();
-    sum
+    Ok(sum)
 }
 
 fn main() {
     let input = include_str!("./input-1.txt");
-    println!("{}", process(input));
+    if let Ok(output) = process(input) {
+        println!("{}", output);
+    }
 }
 
 #[cfg(test)]
@@ -50,6 +51,6 @@ Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
 Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
 Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
 Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11";
-        assert_eq!(process(input), 30);
+        assert_eq!(process(input).unwrap(), 30);
     }
 }
